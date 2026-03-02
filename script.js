625b12383175d7f7b14d17e3cfb674749181148e
Chaitanya Amande <chaitanyaamande@gmail.com>
2026-03-02 23:07:31 +0530

    Add signin and signup button access in script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 // Accessing the elements
 const navbar = document.querySelector(".navbar");
+const signinBtn = document.getElementById("sign_in");
+const signupBtn = document.getElementById("sign_up");
