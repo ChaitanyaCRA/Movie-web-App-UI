93acbfbaa23999d3e8bd559bc01aea8d3413b05d
Chaitanya Amande <chaitanyaamande@gmail.com>
2026-03-02 23:10:03 +0530

    Add searchbar and searchInput to element access
    Added searchbar and searchInput element access.

--- script.js
+++ script.js
@@ -2,3 +2,5 @@
 const navbar = document.querySelector(".navbar");
 const signinBtn = document.getElementById("sign_in");
 const signupBtn = document.getElementById("sign_up");
+const searchbar = document.querySelector("");
+const searchInput = document.getElementById("searchInput");
