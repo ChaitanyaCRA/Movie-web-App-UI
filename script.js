d6846c9da0fe6c7c0341c60e501af2761147839a
Chaitanya Amande <chaitanyaamande@gmail.com>
2026-03-02 23:11:54 +0530

    Update searchbar to searchbox and add searchbtn

--- script.js
+++ script.js
@@ -2,5 +2,7 @@
 const navbar = document.querySelector(".navbar");
 const signinBtn = document.getElementById("sign_in");
 const signupBtn = document.getElementById("sign_up");
-const searchbar = document.querySelector("");
+const searchbox = document.querySelector("search_box");
 const searchInput = document.getElementById("searchInput");
+
+const searchbtn = document.getElementById("searchbtn");
